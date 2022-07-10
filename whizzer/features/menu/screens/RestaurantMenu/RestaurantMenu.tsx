1e48d347d447c6c1c991805384f02850552e1e3a
Alessandro <alessandrofan94@gmail.com>
2022-07-10 14:19:46 +0200

    --wip-- [skip ci]

--- whizzer/features/menu/screens/RestaurantMenu/RestaurantMenu.tsx
+++ whizzer/features/menu/screens/RestaurantMenu/RestaurantMenu.tsx
@@ -1,9 +1,28 @@
-import { View, Text, ScrollView } from "react-native";
+import { Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
+
 const RestaurantMenuScreen = (): JSX.Element => {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn("Tag found", tag);
+    } catch (ex) {
+      console.warn("Oops!", ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
   return (
     <ScrollView>
       <Text>RestaurantMenuScreen</Text>
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Touch me</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
